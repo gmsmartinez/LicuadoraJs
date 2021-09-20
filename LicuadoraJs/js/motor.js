559d7205c04f28a458088f55b17beef5d9244257
Gabriela <55298017+gmsmartinez@users.noreply.github.com>
2021-09-20 01:30:09 -0300

    Add files via upload

---
+++ LicuadoraJs/js/motor.js
@@ -0,0 +1,29 @@
+var estadoLicuadora = "apagada";
+var sonidoLicuadora = document.getElementById("blender-sound");
+var botonLicuadora = document.getElementById("blender-button-sound");
+var licuadora = document.getElementById("blender");
+
+function controlarLicuadora() {
+    if (estadoLicuadora == "apagada") {
+        estadoLicuadora = "encendida";
+        licuadora.classList.add("active");
+        hacerBrrBrr();
+        /* console.log("me prendiste"); */
+    } else {
+        estadoLicuadora = "apagada";
+        hacerBrrBrr();
+        licuadora.classList.remove("active");
+        /* console.log("me apagaste"); */
+    }
+}
+
+function hacerBrrBrr(){
+    if (sonidoLicuadora.paused) {
+        botonLicuadora.play();
+        sonidoLicuadora.play();
+    } else {
+        botonLicuadora.play();
+        sonidoLicuadora.pause();
+        sonidoLicuadora.currentTime = 0;
+    }
+}
